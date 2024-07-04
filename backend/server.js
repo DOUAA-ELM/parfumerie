d0bef93759e8d5218d6ac36bab28c9978323fd9d
Douaa <douaa>
2024-07-04 04:48:17 +0200

    modification

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import data from './data.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import seedRouter from './routes/seedRoutes.js'
+import productRouter from './routes/productRoutes.js'
+import userRouter from './routes/userRoutes.js'
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db')
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
+
+const app = express()
+
+app.use(express.json()) // analyser les corps des requêtes qui contiennent des données JSON.
+app.use(express.urlencoded({ extended: true })) // analyser les corps des requêtes contenant des données URL-encodées,
+
+app.use('/api/seed', seedRouter)
+app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message })
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`)
+})
